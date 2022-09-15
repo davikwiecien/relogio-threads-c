1258ba8577921915b1290e7e448a81668a7b4b09
davikwiecien <66526045+davikwiecien@users.noreply.github.com>
2022-09-14 22:58:24 -0300

    Update arrumar_tempo_baixo.c

--- arrumar_tempo_baixo.c
+++ arrumar_tempo_baixo.c
@@ -8,7 +8,8 @@ void arrumar_tempo_baixo(int tempo_base, int *tempo_1, int *tempo_2) {
     *tempo_1 = dif;
 
     if(tempo_base != 24)
-         *tempo_2 -= (dif == 0) ? div : div + 1;
+         //*tempo_2 -= (dif == 0) ? div : div + 1;
+       *tempo_2 -= div + 1;
 
     return;
 
